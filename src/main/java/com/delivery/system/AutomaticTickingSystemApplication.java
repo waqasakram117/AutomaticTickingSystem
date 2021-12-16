83ec086851d3c9fca7ea8455b730db44377e278e
waqasakram117 <waqasakram117@gmail.com>
2021-12-16 14:33:48 +0500

    Setup project

---
+++ src/main/java/com/delivery/system/AutomaticTickingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticTickingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticTickingSystemApplication.class, args);
+	}
+
+}
